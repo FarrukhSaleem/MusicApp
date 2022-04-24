c63c7ec69f90d6b93ee37bc4ccf5f26efb5ab129
Farrukh Saleem Sheikh <imfarrukhsaleem@hotmail.com>
2022-04-24 02:05:10 -0700

    add migration

--- MusicApp/Controllers/SongsController.cs
+++ MusicApp/Controllers/SongsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MusicApp.Data;
 using MusicApp.Model;
 
 namespace MusicApp.Controllers
@@ -8,24 +9,31 @@ namespace MusicApp.Controllers
 	[ApiController]
 	public class SongsController : ControllerBase
 	{
-		List<Song> songs = new List<Song>() {
+		private ApiDbContext _apiDbContext;
+		public SongsController(ApiDbContext apiDbContext)
+		{
+			this._apiDbContext = apiDbContext;
+		}
 
-			new Song{
-			ID = 1,
-			Language = "en",
-			Title = "My New Song"
-		},new Song {
-		ID = 2,
-			Language = "ur",
-			Title = "Mera New Song"
-		} 
+		//List<Song> songs = new List<Song>() {
+
+		//	new Song{
+		//	ID = 1,
+		//	Language = "en",
+		//	Title = "My New Song"
+		//},new Song {
+		//ID = 2,
+		//	Language = "ur",
+		//	Title = "Mera New Song"
+		//} 
 		
-		};
+		//};
 
 		[HttpGet]
 		public IEnumerable<Song> Get()
 		{
-			return this.songs;
+			return _apiDbContext.Songs;
+
 		}
 	}	
 }
---
+++ MusicApp/Data/ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicApp.Model;
+
+namespace MusicApp.Data
+{
+	public class ApiDbContext:DbContext
+	{
+        public ApiDbContext(DbContextOptions<ApiDbContext> options):base(options)        
+        {
+
+        }
+        
+        public DbSet<Song>? Songs { get; set; }
+	}
+}
