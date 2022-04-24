891b3e7b13098d216d438757aee17e60f157fec5
Farrukh Saleem Sheikh <imfarrukhsaleem@hotmail.com>
2022-04-23 22:03:33 -0700

    Add some data

---
+++ MusicApp/Controllers/SongsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicApp.Model;
+
+namespace MusicApp.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class SongsController : ControllerBase
+	{
+		List<Song> songs = new List<Song>() {
+
+			new Song{
+			ID = 1,
+			Language = "en",
+			Title = "My New Song"
+		},new Song {
+		ID = 2,
+			Language = "ur",
+			Title = "Mera New Song"
+		} 
+		
+		};
+
+		[HttpGet]
+		public IEnumerable<Song> Get()
+		{
+			return this.songs;
+		}
+	}	
+}
--- MusicApp/Model/Song.cs
+++ MusicApp/Model/Song.cs
@@ -2,5 +2,8 @@
 {
 	public class Song
 	{
+		public int ID { get; set; }
+		public string? Title { get; set; }
+		public string? Language { get; set; }
 	}
 }
